6db172ff5ce275fdfdd60e6cf3bbe2ff80033404
Cathy Deng <cathyd125@gmail.com>
2014-11-17 14:25:39 -0600

    tests for manual labeling

--- tests/test_manual_labeling.py
+++ tests/test_manual_labeling.py
@@ -1,31 +1,29 @@
 import training
-from training.manual_labeling import addr2XML
+from training.data_prep.manual_labeling import sequence2XML
 from lxml import etree
 import unittest
 
 
-class TestManualAddr2XML(unittest.TestCase) :
+class TestManualsequence2XML(unittest.TestCase) :
 
     def test_single_component(self) :
 
-        test_input = [ ('Monroe', 'Component') ]
-        expected_xml = '<AddressString><Component>Monroe</Component></AddressString>'
-
-        assert etree.tostring( addr2XML(test_input) ) == expected_xml
+        test_input = [ ('bob', 'foo') ]
+        expected_xml = '<Name><foo>bob</foo></Name>'
+        assert etree.tostring( sequence2XML(test_input) ) == expected_xml
 
     def test_two_components(self) :
 
-        test_input = [ ('123', 'foo'), ('Monroe', 'bar') ]
-        expected_xml = '<AddressString><foo>123</foo> <bar>Monroe</bar></AddressString>'
-
-        assert etree.tostring( addr2XML(test_input) ) == expected_xml
+        test_input = [ ('bob', 'foo'), ('b', 'bar') ]
+        expected_xml = '<Name><foo>bob</foo> <bar>b</bar></Name>'
+        assert etree.tostring( sequence2XML(test_input) ) == expected_xml
 
     def test_multiple_components(self) :
 
-        test_input = [ ('123', 'foo'), ('Monroe', 'bar'), ('St', 'foobar') ]
-        expected_xml = '<AddressString><foo>123</foo> <bar>Monroe</bar> <foobar>St</foobar></AddressString>'
+        test_input = [ ('bob', 'foo'), ('b', 'bar'), ('sr', 'foobar') ]
+        expected_xml = '<Name><foo>bob</foo> <bar>b</bar> <foobar>sr</foobar></Name>'
 
-        assert etree.tostring( addr2XML(test_input) ) == expected_xml
+        assert etree.tostring( sequence2XML(test_input) ) == expected_xml
 
 
 if __name__ == '__main__' :
