b117d098ceda9396aa5bbbcef929b2c920a44a89
Cathy Deng <cathyd125@gmail.com>
2014-12-04 11:52:48 -0600

    update tests

--- tests/test_tokenizing.py
+++ tests/test_tokenizing.py
@@ -1,35 +1,37 @@
-from name_parser import tokenize
+import name_parser
 import unittest
+
+p = name_parser.Parser()
 
 class TestTokenizing(unittest.TestCase) :
 
     def test_split_on_punc(self) :
 
-        assert tokenize('belcher,bob') == ['belcher,', 'bob']
-        assert tokenize('bob foo-bar') == ['bob', 'foo-bar']
+        assert p.tokenize('belcher,bob') == ['belcher,', 'bob']
+        assert p.tokenize('bob foo-bar') == ['bob', 'foo-bar']
     
     def test_spaces(self) :
 
-        assert tokenize('foo bar') == ['foo', 'bar']
-        assert tokenize('foo  bar') == ['foo', 'bar']
-        assert tokenize('foo bar ') == ['foo', 'bar']
-        assert tokenize(' foo bar') == ['foo', 'bar']
+        assert p.tokenize('foo bar') == ['foo', 'bar']
+        assert p.tokenize('foo  bar') == ['foo', 'bar']
+        assert p.tokenize('foo bar ') == ['foo', 'bar']
+        assert p.tokenize(' foo bar') == ['foo', 'bar']
 
     def test_capture_punc(self) :
 
-        assert tokenize('bob b.') == ['bob', 'b.']
-        assert tokenize('bob b., jr') == ['bob', 'b.,', 'jr' ]
-        assert tokenize('bob b. jr') == ['bob', 'b.', 'jr' ]
-        assert tokenize("robert 'bob' belcher") == ['robert', "'bob'", 'belcher']
-        assert tokenize('robert "bob" belcher') == ['robert', '"bob"', 'belcher']
+        assert p.tokenize('bob b.') == ['bob', 'b.']
+        assert p.tokenize('bob b., jr') == ['bob', 'b.,', 'jr' ]
+        assert p.tokenize('bob b. jr') == ['bob', 'b.', 'jr' ]
+        assert p.tokenize("robert 'bob' belcher") == ['robert', "'bob'", 'belcher']
+        assert p.tokenize('robert "bob" belcher') == ['robert', '"bob"', 'belcher']
 
     def test_ampersand(self) :
-        assert tokenize('mr & mrs') == ['mr', '&', 'mrs']
+        assert p.tokenize('mr & mrs') == ['mr', '&', 'mrs']
 
     def test_paren(self) :
-        assert tokenize('robert (bob) belcher') == ['robert', '(bob)', 'belcher']
-        assert tokenize('robert(bob) belcher') == ['robert', '(bob)', 'belcher']
-        assert tokenize('robert (bob)belcher') == ['robert', '(bob)', 'belcher'] 
+        assert p.tokenize('robert (bob) belcher') == ['robert', '(bob)', 'belcher']
+        assert p.tokenize('robert(bob) belcher') == ['robert', '(bob)', 'belcher']
+        assert p.tokenize('robert (bob)belcher') == ['robert', '(bob)', 'belcher'] 
 
 if __name__ == '__main__' :
     unittest.main()    
