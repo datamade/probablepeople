120d79c9dc0d21ef01a4f0be54c5277d5d6dd57d
Cathy Deng <cathyd125@gmail.com>
2014-12-04 14:41:52 -0600

    clean up training

--- training/training.py
+++ training/training.py
@@ -9,10 +9,8 @@ import parserator
 
 if __name__ == '__main__':
 
-    root_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
-
-    training_data = list(parserator.training.readTrainingData(root_path + '/training/training_data/labeled.xml'))
-
     p = name_parser.Parser()
-    parserator.training.trainModel(training_data, root_path + '/name_parser/learned_settings.crfsuite', p)
+
+    parserator.training.train(p)
+    
     reload(name_parser)
