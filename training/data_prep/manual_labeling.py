a73f3fac3199170403e97d3fee3365372d9333eb
Forest Gregg <fgregg@uchicago.edu>
2014-12-04 11:41:39 -0600

    can move argparsing to parserator

--- training/data_prep/manual_labeling.py
+++ training/data_prep/manual_labeling.py
@@ -1,16 +1,8 @@
-from parserator.manual_labeling import getArgumentParser, label
+from parserator.manual_labeling import label
 import name_parser
-from lxml import etree
-import sys
-import os.path
-import data_prep_utils
-import re
-
 
 if __name__ == '__main__' :
 
-    arg_parser = getArgumentParser()
-    args = arg_parser.parse_args()
     p = name_parser.Parser()
 
-    label(p, args, 'training/data_prep/unlabeled_data/')
+    label(p, 'training/data_prep/unlabeled_data/')
