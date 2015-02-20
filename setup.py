bfa331b34c6cae90cd0aaa5d462a0584519910ba
Cathy Deng <cathyd125@gmail.com>
2015-02-20 09:30:35 -0600

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError :
 
 
 setup(
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/datamade/probablepeople',
     description='Parse romanized names using advanced NLP methods',
     name='probablepeople',
