6264ba1389ea007100974d1e31884c9076d84963
Cathy Deng <cathyd125@gmail.com>
2015-03-17 10:04:01 -0500

    update tag test

--- tests/test_tagging.py
+++ tests/test_tagging.py
@@ -5,6 +5,6 @@ class TestTagging(unittest.TestCase) :
 
     def test_basic(self) :
         tagged = tag("Bob Belcher")
-        self.assertEqual("Bob", tagged['GivenName'])
-        self.assertEqual("Belcher", tagged['Surname'])
+        self.assertEqual("Bob", tagged[0]['GivenName'])
+        self.assertEqual("Belcher", tagged[0]['Surname'])
     
