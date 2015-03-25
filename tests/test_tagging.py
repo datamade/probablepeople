d4a96d4d1b57fb526630143543e1dea780a761c9
Forest Gregg <fgregg@uchicago.edu>
2015-03-25 15:29:09 -0500

    reverser order of parsed fields and tag in address

--- tests/test_tagging.py
+++ tests/test_tagging.py
@@ -4,7 +4,7 @@ import unittest
 class TestTagging(unittest.TestCase) :
 
     def test_basic(self) :
-        name_type, tagged = tag("Bob Belcher")
+        tagged, name_type = tag("Bob Belcher")
         assert name_type == 'Person'
         self.assertEqual("Bob", tagged['GivenName'])
         self.assertEqual("Belcher", tagged['Surname'])
