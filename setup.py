fded3b5b06bb60bc57cf864c8955c53b7e554b1d
Nicholas Chammas <nicholas.chammas@gmail.com>
2017-09-18 11:26:08 -0400

    change development status to Beta

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'future>=0.14',
         'doublemetaphone'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
