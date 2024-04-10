db30e51da57c7deb5e6005ca023642cb0f98b4a0
Derek Eder <derek.eder@gmail.com>
2024-04-10 16:09:17 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError :
 
 
 setup(
-    version='0.5.4',
+    version='0.5.5',
     url='https://github.com/datamade/probablepeople',
     description='Parse romanized names & companies using advanced NLP methods',
     name='probablepeople',
